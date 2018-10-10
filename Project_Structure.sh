a235c109f828c80bdb97e2692e35f4f9a5588fca
maithreyi123 <mythrineduri@gmail.com>
2018-10-10 19:31:05 +0530

    added comments

--- Project_Structure.sh
+++ Project_Structure.sh
@@ -1,25 +1,33 @@
+#The code Begins Here
 echo "Welcome Enter the project name"
 read Projectname
+#Creating the Rootfolder with the given name
 mkdir "$Projectname"
 echo "Folder created with name $Projectname"
 cd "$Projectname"
+#Creating the folders based on the web application
 mkdir build dist docs lib setup src test web
 echo "Folders created:- 'build' 'dist' 'docs' 'lib' 'setup' 'src' 'test' 'web'"
 cd src
+#Folders based on web application
 mkdir conf java
 echo "Folders created inside src folder:- 'conf' 'java'"
 cd ..
 cd web
+#Copying the files from Main code the created directory
 cp /home/ec2-user/files/test.html /home/ec2-user/"$Projectname"/web
 cp /home/ec2-user/files/test.jsp /home/ec2-user/"$Projectname"/web
 echo "files copied are test.html and test.jsp"
+#Folders based on web application
 mkdir WEB-INF
 cd WEB-INF
+
 mkdir jspf lib tlds tags
 echo "folders created are 'jspf' 'lib' 'tlds' 'tags'"
 cp /home/ec2-user/files/web.xml /home/ec2-user/"$Projectname"/WEB-INF
 cd ..
 cd ..
+#Copying the files from Main code the created directory
 cp /home/ec2-user/files/prop.properties /home/ec2-user/"$Projectname"
 cp /home/ec2-user/files/build.xml /home/ec2-user/"$Projectname"
 cp /home/ec2-user/files/README.txt /home/ec2-user/"$Projectname"
