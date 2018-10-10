64a355d02715812239ec666cde7f2f6986703c0b
thoopalliamar <amarnaththoopalli97@gmail.com>
2018-10-10 20:10:38 +0530

    sample_project

--- Project_Structure.sh
+++ Project_Structure.sh
@@ -1,3 +1,14 @@
+#Greetings to the USER
+GREETING="Welcome to Project_Structure generator"
+h=`date +%H`
+
+if [ $h -lt 12 ]; then
+  echo Good morning $GREETING 
+elif [ $h -lt 18 ]; then
+  echo Good afternoon $GREETING
+else
+  echo Good evening $GREETING
+fi
 #The code Begins Here
 echo "Welcome Enter the project name"
 read Projectname
@@ -7,11 +18,15 @@ read OLD_PATH
 NEW_PATH=$PWD
 echo $NEW_PATH
 echo "Folder created with name $Projectname"
+
+
+
 cd "$Projectname"
 #Creating the folders based on the web application
 mkdir build dist docs lib setup src test web
 echo "Folders created:- 'build' 'dist' 'docs' 'lib' 'setup' 'src' 'test' 'web'"
 cd src
+TEMP_PATH=src;
 #Folders based on web application
 mkdir conf java
 echo "Folders created inside src folder:- 'conf' 'java'"
@@ -37,4 +52,5 @@ cp $OLD_PATH/files/README.txt $NEW_PATH/"$Projectname"
 cp $OLD_PATH/files/LICENSE.txt $NEW_PATH/"$Projectname"
 echo "Files are copied in the folder"
 echo "Project Structure is created with all  the needed files"
+
 echo "############################Thank you##########################"
